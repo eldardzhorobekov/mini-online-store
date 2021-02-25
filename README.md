712000ae6e164c4ec4755b89e5bd7a34a5d76fe1
mio-eldar <eldar.djorobekov@gmail.com>
2021-02-25 13:03:00 +0600

    Inital commit: Django and Postgresql configured

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # mini-online-store
+
+
+Требования:
+    Python 3.6+
+    Django 2.2+
+    Postgres
+
+Как запустить проект на локальном сервере:
+
+Скачать репозиторий:
+
+    git clone https://github.com/eldardzhorobekov/mini-online-store.git
+    cd mini-online-store
+
+Создать и активировать виртуальное окружение (я использовал [virtualenv](https://pypi.org/project/virtualenv/)):
+    
+    python3 -m virtualenv venv
+    source ./venv/bin/activate # под linux
+
+Установить все необходимые пакеты проекта:
+    
+    pip install -r requirements.txt
+
+Создать .env файл в папке store и указать данные для подключения к базе postgresql:
+    
+    PSQL_DBNAME=<имя базы данных>
+    PSQL_USER=<имя пользователя>
+    PSQL_PASSWORD=<пароль>
+
+Заполнить базу данными:
+    
+    cd store
+    ./manage.py loaddata ./fixtures/db.json
+
+Миграция и запуск:
+    
+    ./manage.py makemigrations
+    ./manage.py migrate
+    ./manage.py runserver
---
+++ store/store/settings.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+import os
+import dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file_path = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file_path):
+    dotenv.load_dotenv(dotenv_file_path)
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret-key')
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'store.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'store.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PSQL_DBNAME'),
+        'USER': os.getenv('PSQL_USER'),
+        'PASSWORD': os.getenv('PSQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
